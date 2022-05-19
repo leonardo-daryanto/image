b7434b44fcff92e52321d3da71e65b72d12ab8dd
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-05-19 13:22:00 +0700

    Update pxt.json, main.blocks, main.ts, custom.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
-basic.forever(function () {
-	
-})
+namespace images {
+    export function showRandomImages(): void {
+        let image = randint(1, 3)
+        
+        if (image == 1) {
+            
+            basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        }
+
+        if (image == 2) {
+            
+            basic.showIcon(IconNames.Heart)
+        }
+        if (image == 3) {
+
+            basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        
+        }
+    }
+}
+let item: number
